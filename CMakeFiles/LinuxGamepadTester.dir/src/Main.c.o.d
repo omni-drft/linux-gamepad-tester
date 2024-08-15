ae9fdb5255fb3509fc1b225055b53471204fc7ce
omni-drft <piotrwalczak.pv@gmail.com>
2024-08-15 16:50:08 +0200

    Trying to apply new gitignore

--- CMakeFiles/LinuxGamepadTester.dir/src/Main.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/LinuxGamepadTester.dir/src/Main.c.o: \
- /run/media/omni-drft/6166-3131/dev/c/linux-gamepad-tester/src/Main.c \
- /usr/include/stdc-predef.h /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdarg.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdbool.h \
- /run/media/omni-drft/6166-3131/dev/c/linux-gamepad-tester/src/InputHandler.h
